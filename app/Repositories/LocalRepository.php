019a5bc6c172ae64704c91d4329dac43185f51e5
Marcelo Schneider <schneider.fei@gmail.com>
2018-10-05 18:00:55 -0300

    Adicionado o Controller para Local - issue2

---
+++ app/Repositories/LocalRepository.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Repositories;
+
+use Prettus\Repository\Contracts\RepositoryInterface;
+
+/**
+ * Interface LocalRepository.
+ *
+ * @package namespace App\Repositories;
+ */
+interface LocalRepository extends RepositoryInterface
+{
+    //
+}
