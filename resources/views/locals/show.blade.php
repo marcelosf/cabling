5f499182c82d25be0869aef20388167033f96359
Marcelo Schneider <schneider.fei@gmail.com>
2018-10-08 15:08:40 -0300

    Deletando Local - issue2
    Adicinando botão para deletar um local

--- resources/views/locals/show.blade.php
+++ resources/views/locals/show.blade.php
@@ -2,3 +2,11 @@
 <p>Andar: {{ $local->floor }}</p>
 <p>Local: {{ $local->local }}</p>
 <p>Criado em: {{ $local->created_at->format('d/m/Y') }}</p>
+
+<form action="{{ route('locals.destroy', ['id' => $local->id]) }}" method="POST">
+
+    @csrf    
+    @method('DELETE')
+
+    <button type="submit">Remover</button>
+</form>
