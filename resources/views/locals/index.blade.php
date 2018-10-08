9831b07b0c2bf2166018c88fc1d92e6306e8b3a8
Marcelo Schneider <schneider.fei@gmail.com>
2018-10-08 13:46:14 -0300

    Link para detalhes dos Locais - issue2
    Adicionando a coluna Detalhes na lista de locais com link para
    visualização dos detalhes para cada local

--- resources/views/locals/index.blade.php
+++ resources/views/locals/index.blade.php
@@ -5,6 +5,7 @@
                 <th>Bloco</th>
                 <th>Andar</th>
                 <th>Local</th>
+                <th>Detalhes</th>
             </tr>
             <tbody>
             @foreach($locals as $local)
@@ -12,6 +13,7 @@
                     <td>{{ $local->build }}</td>
                     <td>{{ $local->floor }}</td>
                     <td>{{ $local->local }}</td>
+                    <td><a href="{{ route('locals.show', ['id' => $local->id]) }}">Ver</a>
                 </tr>
             @endforeach
             </tbody>
