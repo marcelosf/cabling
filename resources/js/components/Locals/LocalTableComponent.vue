a5e0cf63e91dc58199bf7e976251da5f24c749f8
Marcelo Schneider <schneider.fei@gmail.com>
2018-10-11 14:57:45 -0300

    Listando os locais-issue #42

--- resources/js/components/Locals/LocalTableComponent.vue
+++ resources/js/components/Locals/LocalTableComponent.vue
@@ -11,14 +11,64 @@
             </tr>
         </thead>
         <tbody>
-            
+            <tr v-for="local in locals" :key="local.id">
+                <td>{{ local.build }}</td>
+                <td>{{ local.floor }}</td>
+                <td>{{ local.local }}</td>
+                <td>
+                    <a :href="'/locals/' + local.id">
+                        <i class="material-icons">subject</i>
+                    </a>
+                </td>
+                <td>
+                    <a :href="'/locals/' + local.id + '/edit'">
+                        <i class="material-icons">edit</i>
+                    </a>
+                </td>
+            </tr>
         </tbody>
     </table>
 
 </template>
 
 <script>
+    import {LocalResource} from '../resources/LocalResource';
+
     export default {
-       
+
+        mounted () {
+
+            this.list(response => {
+
+                this.locals = response.data.data;
+
+            }, 1);
+
+        },
+
+        data () {
+
+            return {
+                
+                locals: [],
+                baseUrl: process.env.MIX_API_BASEURL
+
+            }
+
+        },
+
+        methods: {
+
+            list (action, page) {
+
+                LocalResource.index(response => {
+
+                    action(response);
+
+                }, page);
+
+            }
+
+        }
     }
 </script>
