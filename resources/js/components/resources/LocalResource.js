87fe0721d9610a28275d8cd7a45a700ef7fba4ba
Marcelo Schneider <schneider.fei@gmail.com>
2018-10-11 14:58:51 -0300

    Corrigindo assinatura do da função index-issue #42

--- resources/js/components/resources/LocalResource.js
+++ resources/js/components/resources/LocalResource.js
@@ -4,14 +4,14 @@ const ELEMENT = 'locals';
 
 export class LocalResource extends Resource {
 
-    static index (action, search, errors) {
+    static index (action, page, search, errors) {
 
         let query = this.generateQuery(search);
 
         this._getApi().get(ELEMENT + '?page=' + page + query).then((response) => {
 
             action(response.data);
-            
+
         }).catch(errors);
 
     }
