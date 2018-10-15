0ad4351f9802dc9e824aff84659829b47e297b69
Marcelo Schneider <schneider.fei@gmail.com>
2018-10-15 12:59:26 -0300

    Incluindo csrf no axios-issue #42

--- resources/js/components/resources/Api.js
+++ resources/js/components/resources/Api.js
@@ -12,7 +12,9 @@ export class Api {
     initialize () {
         
         let instance = axios.create();
-        
+
+        instance.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+        instance = this._setCsrfToken(instance);
         instance.defaults.baseURL = this.baseUrl;
         instance.defaults.timeout = 200000;
 
@@ -33,6 +35,19 @@ export class Api {
             return true;
 
         }
+    }
+
+    _setCsrfToken (instance) {
+        
+        let token = document.head.querySelector('meta[name="csrf-token"]');
+        
+        if (token) {
+            instance.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+        } else {
+            console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+        }
+
+        return instance;
     }
 
 }
