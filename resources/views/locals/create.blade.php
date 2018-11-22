a4791f6c0704bf33e5972cf0b28b0a0b63edaa8f
Marcelo Schneider <schneider.fei@gmail.com>
2018-11-22 17:15:13 -0200

    Remoção do formulário blade - issue #40
    Remoção do formulário blade e utilização do formulário criado por meio
    do Vuejs

--- resources/views/locals/create.blade.php
+++ resources/views/locals/create.blade.php
@@ -16,41 +16,6 @@
 
 @section('content')
 
-    <div class="card">
-
-        <form action="{{ route('locals.store') }}" method="POST">   
-        
-            <div class="card-content">
-
-                @csrf
-
-                <p>
-                    <label for="local-build">Bloco:</label>
-                    <input type="text" name="build" id="local-build">
-                </p>
-                <p>
-                    <label for="local-floor">Andar:</label>
-                    <input type="text" name="floor" id="local-floor">
-                </p>    
-                <p>
-                    <label for="local-local">Local:</label>
-                    <input type="text" name="local" id="local-local">
-                </p>
-
-            </div>
-
-            <div class="card-action">
-
-                <button class="btn-flat waves-effect waves-light blue-text" type="submit">
-                    <i class="material-icons right">send</i> Salvar
-                </button>
-
-            </div>
-
-        </form>
-
-    </div>
-
     <localform-component resource="create"></localform-component>
 
 @endsection
