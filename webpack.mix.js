720774e78fd6596839331e46f78cc7db6696e6b0
Marcelo Schneider <schneider.fei@gmail.com>
2018-12-20 18:39:09 -0200

    Adicionando estilo-issue#68
    Adicionando estilo para a página principal

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,6 +14,7 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js');
 mix.js('resources/js/locals/local.js', 'public/js');
 mix.sass('resources/sass/app.scss', 'public/css');
+mix.sass('resources/sass/welcome.scss', 'public/css');
 mix.version();
 mix.browserSync({
     proxy: 'nginx',
