7d05ec70790a75777c6c526096f2365233f9799b
Marcelo Schneider <schneider.fei@gmail.com>
2018-12-21 13:37:14 -0200

    Criação do template cards-issue#69
    Criação do template cards para reutilização

---
+++ resources/views/layouts/cards.blade.php
@@ -0,0 +1,12 @@
+<div class="card hoverable">
+    <div class="card-image">
+        <img src="{{ $image }}">
+        <span class="card-title">{{ $title }}</span>
+    </div>
+    <div class="card-content">
+        <p>{{ $content }}</p>
+    </div>
+    <div class="card-action">
+        <a href="{{ $actionTarget }}">{{ $actionText }}</a>
+    </div>
+</div>
