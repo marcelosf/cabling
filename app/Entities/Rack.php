3d7643ab2caec8ce2d56d2656e420f279caa04eb
Marcelo Schneider <schneider.fei@gmail.com>
2019-01-07 14:14:57 -0200

    Adicionando fillables na Entity Racks - issue#3
    Adionando os campos fillables na entity Racks

--- app/Entities/Rack.php
+++ app/Entities/Rack.php
@@ -20,6 +20,10 @@ class Rack extends Model implements Transformable
      *
      * @var array
      */
-    protected $fillable = [];
+    protected $fillable = [
+        'name',
+        'size',
+        'local_id',
+    ];
 
 }
