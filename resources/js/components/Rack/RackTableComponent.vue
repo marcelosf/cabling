43bde02de81a751e11e787ff45a7e92328df5834
Marcelo Schneider <schneider.fei@gmail.com>
2019-01-10 10:25:15 -0200

    Adicionando lista de Racks - issue#3

---
+++ resources/js/components/Rack/RackTableComponent.vue
@@ -0,0 +1,101 @@
+<template>
+    
+    <div>
+
+        <table class="highlight">
+            <thead>
+                <tr>
+                    <th>Nome</th>
+                    <th>Bloco</th>
+                    <th>Sala</th>
+                    <th>Detalhes</th>
+                    <th>Editar</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="local in locals" :key="local.id">
+                    <td>{{ local.build }}</td>
+                    <td>{{ local.floor }}</td>
+                    <td>{{ local.local }}</td>
+                    <td>
+                        <a :href="'/locals/' + local.id">
+                            <i class="material-icons">subject</i>
+                        </a>
+                    </td>
+                    <td>
+                        <a :href="'/locals/' + local.id + '/edit'">
+                            <i class="material-icons">edit</i>
+                        </a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+
+        <pagination></pagination>
+
+    </div>
+
+
+</template>
+
+<script>
+    import {RackResource} from '../resources/RackResource';
+    import PaginationComponent from '../Pagination/PaginationComponent';
+
+    export default {
+
+        mounted () {
+
+            this.list(response => {
+
+                this.commitTableData(response.data);
+
+            }, 1);
+
+        },
+
+        data () {
+
+            return {
+                
+                data: []
+
+            }
+
+        },
+
+        computed: {
+
+            locals () {
+
+                return this.$store.getters['table/tableData'];
+
+            }
+
+        },
+
+        methods: {
+
+            list (action, page) {
+
+                RackResource.index(response => {
+
+                    action(response);
+
+                }, page);
+
+            },
+
+            commitTableData (data) {
+
+                this.$store.commit('table/tableData', data);
+
+            }
+
+        },
+
+        components: {
+            'pagination': PaginationComponent
+        }
+    }
+</script>
