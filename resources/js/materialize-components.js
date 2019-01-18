44d79001390f82e626ed999f953537f2c0092a61
Marcelo Schneider <schneider.fei@gmail.com>
2019-01-18 13:57:25 -0200

    Corrigindo trigger para dropdown-issue#3

--- resources/js/materialize-components.js
+++ resources/js/materialize-components.js
@@ -3,11 +3,6 @@ $(document).ready(function() {
     // Collapsible component
     $('.collapsible').collapsible();
 
-    // Dropdown
-    $('.dropdown-trigger').dropdown({
-        coverTrigger: false,
-    });
-
     // Select
     $('select').formSelect();
 });
