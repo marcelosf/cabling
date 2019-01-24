b3111b835e46599d0cec8849cefa7185d30f02b0
Marcelo Schneider <schneider.fei@gmail.com>
2019-01-24 11:25:12 -0200

    Corrigindo factory para patches-issue#4

--- database/factories/PatchesFactory.php
+++ database/factories/PatchesFactory.php
@@ -9,12 +9,12 @@ $factory->define(Model::class, function (Faker $faker) {
     
     $racks = Rack::all()->pluck('id');
     $locals = Local::all()->pluck('id');
-    
+
     return [
         'label' => $faker->regexify('^[0-9]{2}[A-Z]-[0-9]{2}-[0-9]{2}?'),
         'number' => $faker->randomNumber(2),
         'switch_port' => $faker->randomNumber(2),
-        'rack_id' => $faker->randomElement($racks['all']),
-        'local_id' => $faker->randomElement($locals['all']),
+        'rack_id' => $faker->randomElement($racks),
+        'local_id' => $faker->randomElement($locals),
     ];
 });
