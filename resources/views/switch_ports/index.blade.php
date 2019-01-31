4209b9bd845f035ecc65ec8dd710adc430b49488
Marcelo Schneider <schneider.fei@gmail.com>
2019-01-31 15:42:27 -0200

    Criando view para lista de Switches-issue#5

---
+++ resources/views/switch_ports/index.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.main')
+
+@section('scripts')
+        <!-- Scripts -->
+        <script src="{{ mix('js/local.js') }}" defer></script>
+@endsection
+
+@section('pageTitle')
+<div class="z-depth-1">
+            
+            <div class="row">
+
+                <span class="btn-large">Lista de Switches</span>
+
+                <rackfilter-component class="right"></rackfilter-component>
+    
+                <a href="{{ route('switch_port.create') }}" class="btn btn-large btn-flat right">
+                    Novo <i class="material-icons right">add</i>
+                </a>
+    
+            </div>
+            
+        </div>   
+@endsection
+
+@section('content')
+
+    <div class="card">
+        <div class="card-content">
+            
+        </div>
+    </div>
+
+@endsection
