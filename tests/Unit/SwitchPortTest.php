3a754ec83a1e48868a5d28c2365db93c20ec1a7c
Marcelo Schneider <schneider.fei@gmail.com>
2019-02-04 17:56:01 -0200

    Testando atualização-issue#5
    Testando atualização da Porta do Switch

--- tests/Unit/SwitchPortTest.php
+++ tests/Unit/SwitchPortTest.php
@@ -6,6 +6,7 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use App\User;
+use App\Entities\SwitchPort;
 
 
 
@@ -26,6 +27,33 @@ class SwitchPortTest extends TestCase
         $response = $this->actingAs($this->user)->json('GET', '/switch-port');
         $response->assertOk();
         $response->assertJson(['data' => ['data' => []]]);
+    }
+
+    /** @test */
+    public function update_switch_port_data()
+    {
+        $switchPort = [
+            'port_number' => '1',
+            'poe' => 0,
+            'poe_status' => 0,
+            'vlan' => '20',
+            'switch_name' => 'cacilds',
+            'switch_brand' => 'cacilds',
+            'switch_code' => 'cacilds',
+            'stack_name' => 'cacilds',
+            'stack_ip' => '1.0.0.10',
+            'rack_id' => 1,
+        ];
+        
+        $switchPortUpdated = $switchPort;
+        $switchPortUpdated['switch_name'] = 'cacilds_1';
+        $s = factory(SwitchPort::class)->create($switchPort);
+
+        $uri = '/switch-port/' . $s->id;
+        $response = $this->actingAs($this->user)->json('PUT', $uri, $switchPortUpdated);
+        $response->assertOk();
+        $response->assertJson(['message' => 'Porta do Switch Atualizada']);
+
     }
 
 }
