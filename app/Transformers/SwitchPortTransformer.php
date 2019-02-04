7ab00714098b684f280e1e8955f4548ef55824c7
Marcelo Schneider <schneider.fei@gmail.com>
2019-02-04 13:37:20 -0200

    Criando Transformer para Switch Ports-issue#5

---
+++ app/Transformers/SwitchPortTransformer.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Transformers;
+
+use League\Fractal\TransformerAbstract;
+use App\Entities\SwitchPort;
+
+/**
+ * Class SwitchPortTransformer.
+ *
+ * @package namespace App\Transformers;
+ */
+class SwitchPortTransformer extends TransformerAbstract
+{
+    /**
+     * Transform the SwitchPort entity.
+     *
+     * @param \App\Entities\SwitchPort $model
+     *
+     * @return array
+     */
+    public function transform(SwitchPort $model)
+    {
+        return [
+            'id' => (int) $model->id,
+            'port_number' => $model->port_number,
+            'poe' => $model->poe,
+            'poe_status' => $model->poe_status,
+            'vlan' => $model->vlan,
+            'switch_name' => $model->switch_name,
+            'switch_brand' => $model->parent,
+            'switch_code' => $model->switch_code,
+            'stack_name' => $model->stack_name,
+            'stack_ip' => $model->stack_ip,
+            'rack_id' => $model->rack_id,
+            'rack' => $model->rack->name,
+            'created_at' => $model->created_at,
+            'updated_at' => $model->updated_at
+        ];
+    }
+}
