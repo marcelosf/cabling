4003593e54f6e72bd15f3ffc652d96f343cd8c96
Marcelo Schneider <schneider.fei@gmail.com>
2019-02-06 18:33:07 -0200

    Criando Repository-issue#7
    Criando repository para voice port

---
+++ app/Repositories/VoicePortRepositoryEloquent.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Repositories;
+
+use Prettus\Repository\Eloquent\BaseRepository;
+use Prettus\Repository\Criteria\RequestCriteria;
+use App\Repositories\VoicePortRepository;
+use App\Entities\VoicePort;
+use App\Validators\VoicePortValidator;
+
+/**
+ * Class VoicePortRepositoryEloquent.
+ *
+ * @package namespace App\Repositories;
+ */
+class VoicePortRepositoryEloquent extends BaseRepository implements VoicePortRepository
+{
+    /**
+     * Specify Model class name
+     *
+     * @return string
+     */
+    public function model()
+    {
+        return VoicePort::class;
+    }
+
+    /**
+    * Specify Validator class name
+    *
+    * @return mixed
+    */
+    public function validator()
+    {
+
+        return VoicePortValidator::class;
+    }
+
+    public function presenter()
+    {
+        return 'App\Presenters\VoicePortPresenter';
+    }
+
+
+    /**
+     * Boot up the repository, pushing criteria
+     */
+    public function boot()
+    {
+        $this->pushCriteria(app(RequestCriteria::class));
+    }
+    
+}
