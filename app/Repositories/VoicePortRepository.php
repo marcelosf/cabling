edbe65d8c846b5372c2180af81738c3324e8a9cc
Marcelo Schneider <schneider.fei@gmail.com>
2019-02-06 17:13:34 -0200

    Criando Repository-issue#7
    Criando repository para Voice port

---
+++ app/Repositories/VoicePortRepository.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Repositories;
+
+use Prettus\Repository\Contracts\RepositoryInterface;
+
+/**
+ * Interface VoicePortRepository.
+ *
+ * @package namespace App\Repositories;
+ */
+interface VoicePortRepository extends RepositoryInterface
+{
+    //
+}
