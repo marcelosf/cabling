79b71cb1d538607df9d7603a0c518a303aa8abc5
Marcelo Schneider <schneider.fei@gmail.com>
2019-02-13 14:09:57 -0200

    Adicionando relations em phone-issue#8
    Adicionando relations na Entity Phones.

--- app/Entities/Phone.php
+++ app/Entities/Phone.php
@@ -27,4 +27,14 @@ class Phone extends Model implements Transformable
         'switchport_id',
     ];
 
+    public function voicepanel()
+    {
+        return $this->belongsTo('App\Entities\VoicePort', 'voicepanel_id');
+    }
+
+    public function switchport()
+    {
+        return $this->belongsTo('App\Entities\SwitchPort', 'switchport_id');
+    }
+
 }
