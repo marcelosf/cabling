e7c199bfc366a3a08aeca01c7838535f8196fc05
Marcelo Schneider <schneider.fei@gmail.com>
2019-02-13 15:26:43 -0200

    Testando função store do controller-issue#8

--- tests/Unit/PhoneTest.php
+++ tests/Unit/PhoneTest.php
@@ -6,6 +6,9 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use App\User;
+use App\Entities\Phone;
+use App\Entities\VoicePort;
+use App\Entities\SwitchPort;
 
 class PhoneTest extends TestCase
 {
@@ -22,5 +25,27 @@ class PhoneTest extends TestCase
         $response = $this->actingAs($this->user)->json('GET', '/phones');
         $response->assertOk();
         $response->assertJson(['data' => ['data' => []]]);
+    }
+
+    public function testStore()
+    {
+        $data = $this->dummyPhone();
+
+        $response = $this->actingAs($this->user)->json('POST', '/phones', $data);
+        $response->assertOk();
+        $response->assertJsonFragment($data);
+    }
+
+    private function dummyPhone()
+    {
+        $voicepanel = factory(VoicePort::class)->create();
+        $switchport = factory(SwitchPort::class)->create();
+
+        return [
+            'number' => '2222',
+            'category' => 'interno',
+            'voicepanel_id' => $voicepanel->id,
+            'switchport_id' => $switchport->id,
+        ];
     }
 }
